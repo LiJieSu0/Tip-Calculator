bd08dc7dc2437941a9b1057dcbac34687b11f02c
LiJie <opwinopwin@gmail.com>
2023-11-06 13:15:57 -0600

    basic function init

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   const [total, setTotal]=useState(0);
+  const [splitPeople,setSplitPeople]=useState(1);
+  const [tipPercent,setTipPercent]=useState(0);
   return (
     <View style={styles.container}>
       <View>
@@ -13,7 +15,21 @@ export default function App() {
             setTotal(val);
           }}
         />
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Tip  
+          <TextInput
+            placeholder='Enter percent you want to pay'
+            onChangeText={(val)=>setTipPercent(val)}
+          /> % 
+        </Text>
+        <Text>Tip= {total*tipPercent/100}</Text>
+        <TextInput
+          placeholder='Enter Split People'
+          onChangeText={(val)=>{
+            setSplitPeople(val);
+          }}
+        />
+        <Text>Splited by {splitPeople}, Everyone pays {(total*(tipPercent/100))/splitPeople} tip</Text>
+        <Text>Everyone pays {total*(1+tipPercent/100)/splitPeople}</Text>
       </View>
     </View>
   );
