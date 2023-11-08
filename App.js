294a59a657b83e7a7a5b7d3310e0f7cc61638e92
LiJieSu0 <opwinopwin@gmail.com>
2023-11-07 22:36:22 -0600

    change to center

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, Text, View, TextInput} from 'react-native';
 import React,{useState} from 'react';
 import Calculator from './components/Calculator';
 import { myData } from './GlobalContext';
+import { globalStyles } from './GlobalStyle';
 export default function App() {
 
   const [value,setValue]=useState(1);
   return (
     <myData.Provider value={value}>
       {/* <View style={globalStyles.container}> */}
-      <View>
+      <View style={globalStyles.container}>
         <Calculator/>
       </View>
     </myData.Provider>
