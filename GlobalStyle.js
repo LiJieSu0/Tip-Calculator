9595a3b109d4eb5497a0d2cb3ec933617d969396
FFVIII <FangVsYu@gmail.com>
2023-11-07 18:51:21 -0600

    update globalstyle file

--- GlobalStyle.js
+++ GlobalStyle.js
@@ -1,9 +1,46 @@
 import { StyleSheet} from 'react-native';
 
-export const GlobalStyle=StyleSheet.create({
+export const globalStyles  = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     button:{
         borderWidth:1,
         borderColor:"black",
-        
+    },
+    font:{
+        fontSize: 16,
+    },
+    wrapperCustom: {
+        borderRadius: 8,
+        padding: 6,
+    },
+    input:{
+        borderWidth:1,
+        borderColor:'black',
+        marginTop:5,
+    },
+    colorStyle:{
+        color:'green'
+    },
+    inputLabelContainer:{
+        flex:1,
+        backgroundColor:'yellow',
+        flexDirection:'row',
+        alignItems:'center',
+        marginBottom:10,
+    },
+    btnContainer:{
+        flex:1,
+        backgroundColor:'green',
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    wrapperCustom: {
+        borderRadius: 8,
+        padding: 6,
     }
 });
