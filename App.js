864663e422d46fff130a1ccacf327b123a07d40e
FFVIII <FangVsYu@gmail.com>
2023-11-07 18:58:11 -0600

    update app.js file

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
 // import { StatusBar } from 'expo-status-bar';
-import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput} from 'react-native';
+import React,{useState} from 'react';
 import Calculator from './components/Calculator';
-
 import { myData } from './GlobalContext';
 export default function App() {
 
   const [value,setValue]=useState(1);
   return (
     <myData.Provider value={value}>
-      <View style={styles.container}>
+      {/* <View style={globalStyles.container}> */}
+      <View>
         <Calculator/>
       </View>
     </myData.Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  font:{
-    fontSize: 16,
-  }
-});
+
